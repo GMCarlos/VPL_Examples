0e1e20ded6bfdd4f91f1990b8d21a09cd25d7ccb
GMCarlos <44506123+GMCarlos@users.noreply.github.com>
2020-12-11 11:52:19 +0100

    Add files via upload

---
+++ ExpresionRegular2/expReg2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class expReg2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero = 0;
+		//System.out.println("Introduzca nombre: ");
+		Scanner reader = new Scanner(System.in);
+	
+		numero = reader.nextInt();
+		if(numero == 0) {
+			System.out.println("guillen");
+		}
+	}
+
+}
