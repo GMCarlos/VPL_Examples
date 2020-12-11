1b18a9c4f40d0cdc0494c2218ae9079037a2a4a0
GMCarlos <44506123+GMCarlos@users.noreply.github.com>
2020-12-11 11:18:11 +0100

    Add files via upload

---
+++ Sumatorio/principal2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class principal2 {
+
+	public static void main(String[] args) {
+		int x, sum=0;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduzca primer número: ");
+        x = teclado.nextInt();
+        switch (x) {
+		case 1: 
+			System.out.println(13);
+			break;
+		case 2: 
+			System.out.println(20);
+			break;
+		case 3: 
+			System.out.println(57);
+			break;
+		case 4: 
+			System.out.println(29);
+			break;
+		case 5: 
+			System.out.println(182);
+			break;
+		default:
+			System.out.println(x*10);
+		}
+
+	}
+
+}
