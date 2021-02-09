226a0ec36729d2d210e5b5535692a6e0e53ab016
GMCarlos <44506123+GMCarlos@users.noreply.github.com>
2021-02-09 11:48:57 +0100

    Update Ideone.java

--- RangoNumeros/Ideone.java
+++ RangoNumeros/Ideone.java
@@ -1,19 +1,22 @@
 import java.util.*;
 
+
+
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone {
 	public static double getRandom(Double valorMinimo, Double valorMaximo) {
 		Random rand = new Random();
+		//rand.nextDouble()
+		//Devuelve un número de punto flotante aleatorio
+		//que es mayor o igual que 0,0 y menor que 1,0
 		return valorMinimo + (valorMaximo - valorMinimo) * rand.nextDouble();
 	}
 
 	public static void main(String[] args) throws java.lang.Exception {
-		int numero = 0;
+		Double numero = 0.0;
 		Scanner reader = new Scanner(System.in);
-		numero = reader.nextInt();
-		if(numero == 4) {
-			System.out.println(String.valueOf(getRandom(4.0, 6.0)));
-		}
+		numero = reader.nextDouble();
+		System.out.println(String.valueOf(getRandom(0.0, numero)));
 		
 
 	}
